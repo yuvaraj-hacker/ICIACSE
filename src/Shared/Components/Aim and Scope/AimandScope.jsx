ae70054a0cd06c86b94361b4d990793262962a80
vetri <vetrivel.tcz@gmail.com>
2025-06-28 18:22:07 +0530

    new

---
+++ src/Shared/Components/Aim and Scope/AimandScope.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LastSection from '../Last/LastSection';
+import Other from '../Other/Other';
+
+function AimandScope() {
+    return (
+        <>
+            <Other Title="Scope of the Conference" />
+            <section className="flex flex-col  md:gap-10 gap-5  max-w-[80rem]  md:py-10 py-5 mx-auto md:px-5 px-3 ">
+                <section className="grid md:grid-cols-2 grid-cols-1 md:gap-5 gap-3 items-center">
+                    <div className='flex flex-col md:gap-5 gap-3'>
+                        {/* <h1 className="text-center font-semibold text-[#269C52] lg:text-2xl text-lg">
+                                Scope of the Conference
+                        </h1> */}
+                        <div className="flex flex-col gap-5">
+                            <p className="text-justify md:text-lg text-sm leading-7">
+                                The scope of ICTIGD  encompasses a wide range of disciplines and technologies with a common goal — leveraging innovation for sustainable and inclusive global development. The conference serves as a forum for the exchange of ideas, breakthroughs, and applications that demonstrate how emerging technologies can address critical challenges in diverse sectors.                            </p>
+                        </div>
+                    </div>
+                    <div className=" overflow-hidden">
+                        <img className="h-64 sm:h-80 w-full object-cover" rel="preload" src="/assets/images/17690.jpg" style={{ clipPath: 'polygon(0 0, calc(100% - 60px) 0, 100% 60px, 100% 100%, 60px 100%, 0 calc(100% - 60px))' }} alt="Conference Visual" />
+                    </div>
+                </section>
+                <section className="">
+                    <div className="mx-auto text-justify text-gray-700 space-y-4 text-sm md:text-base leading-7">
+                        <h1 className="text-center w-fit mx-auto rounded-full font-semibold lg:text-3xl text-xl bg-gradient-to-r p-1 from-[#2487DB] to-[#014BBD] bg-clip-text text-transparent">
+                            vision
+                        </h1>
+                        <div className='grid md:grid-cols-2 gap-7'>
+                            <div className='flex flex-col gap-3 justify-center'>
+                                <h2 style={{
+                                    clipPath: 'polygon(0 0, calc(100% - 10px) 0, 100% 10px, 100% 100%, 10px 100%, 0 calc(100% - 10px))',
+                                }} className='md:text-xl font-semibold bg-gradient-to-r from-[#2487DB] to-[#014BBD]  text-white  md:p-4 p-3  tracking-tight'>Artificial Intelligence and Machine Learning for Development </h2>
+                                <ul className='list-disc ml-8 md:text-lg text-sm leading-7'>
+                                    <li> <i class="fi fi-sc-wifi-1"></i>  Predictive analytics for healthcare, education, agriculture  </li>
+                                    <li>AI for decision-making in policy and governance</li>
+                                    <li>Ethics and fairness in AI for underserved populations</li>
+                                </ul>
+                            </div>
+                            <div className='flex flex-col gap-3'>
+                                <h2 style={{
+                                    clipPath: 'polygon(0 0, calc(100% - 10px) 0, 100% 10px, 100% 100%, 10px 100%, 0 calc(100% - 10px))',
+                                }} className='md:text-xl font-semibold bg-gradient-to-r from-[#2487DB] to-[#014BBD]  text-white md:p-4 p-3   tracking-tight'>Smart Cities and Urban Transformation </h2>
+                                <ul className='list-disc ml-8 md:text-lg text-sm leading-7' >
+                                    <li> Intelligent transportation and mobility
+                                    </li>
+                                    <li>Urban sustainability through IoT and automation</li>
+                                    <li>Data-driven governance and smart infrastructure</li>
+                                </ul>
+                            </div>
+                            <div className='flex flex-col gap-3'>
+                                <h2 style={{
+                                    clipPath: 'polygon(0 0, calc(100% - 10px) 0, 100% 10px, 100% 100%, 10px 100%, 0 calc(100% - 10px))',
+                                }} className='md:text-xl font-semibold bg-gradient-to-r from-[#2487DB] to-[#014BBD]  text-white md:p-4 p-3   tracking-tight'>Sustainable and Green Technologies </h2>
+                                <ul className='list-disc ml-8 md:text-lg text-sm leading-7'>
+                                    <li>Renewable energy systems and smart grids
+                                    </li>
+                                    <li>Circular economy models and eco-innovation</li>
+                                    <li>Low-carbon solutions and climate resilience tools</li>
+                                </ul>
+                            </div>
+                            <div className='flex flex-col gap-3'>
+                                <h2 style={{
+                                    clipPath: 'polygon(0 0, calc(100% - 10px) 0, 100% 10px, 100% 100%, 10px 100%, 0 calc(100% - 10px))',
+                                }} className='md:text-xl font-semibold bg-gradient-to-r from-[#2487DB] to-[#014BBD]  text-white  md:p-4 p-3   tracking-tight'>Internet of Things (IoT) and Embedded Systems </h2>
+                                <ul className='list-disc ml-8 md:text-lg text-sm leading-7'>
+                                    <li>IoT for agriculture, water management, and disaster response
+                                    </li>
+                                    <li>Smart devices in healthcare and home automation</li>
+                                    <li>Sensor networks for real-time monitoring
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </section>
+            <LastSection />
+        </>
+    );
+}
+
+export default AimandScope;
---
+++ src/Shared/Components/Home/Home.jsx
@@ -0,0 +1,234 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import KeyDates from "./KeyDates";
+import LastSection from "../Last/LastSection";
+import Invitees from "./Invitees";
+import Marquee from "react-fast-marquee";
+import { toast } from "react-toastify";
+
+export const Home = () => {
+  const [formData, setFormData] = useState({ name: "", email: "", number: "", message: "" });
+  const [status, setStatus] = useState('');
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus('Sending...');
+    try {
+      const formDataToSend = new FormData();
+      formDataToSend.append('name', formData.name);
+      formDataToSend.append('email', formData.email);
+      formDataToSend.append('number', formData.number);
+      formDataToSend.append('message', formData.message);
+      const response = await fetch('http://192.168.29.174/my-react-app/message_mail.php', {
+        method: 'POST',
+        body: formDataToSend,
+      });
+
+      if (response.ok) {
+        const result = await response.text();
+        setStatus(result);
+        setFormData({
+          name: '',
+          email: '',
+          number: '',
+          message: '',
+        });
+        toast.success("Paper submitted successfully!");
+        console.log("success")
+      } else {
+        setStatus('Failed to send submission. Please try again.');
+        toast.error('Failed to send submission. Please try again.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setStatus('An error occurred. Please try again.');
+      toast.error('An error occurred. Please try again.');
+    }
+  };
+
+  const invitees = [
+    {
+      location: 'location',
+      Name: 'Name',
+      Position: 'Position',
+    },
+    {
+      location: 'location',
+      Name: 'Name',
+      Position: 'Position',
+    },
+    {
+      location: 'location',
+      Name: 'Name',
+      Position: 'Position',
+    },
+    {
+      location: 'location',
+      Name: 'Name',
+      Position: 'Position',
+    },
+
+  ];
+
+  const date = [
+    { Dates: '22-07-2025', Title: 'Full Paper Submission Deadline' },
+    { Dates: '29-07-2025', Title: 'Notification of Decision' },
+    { Dates: '05-08-2025', Title: 'Deadline for Submitting Revised Full Paper' },
+    { Dates: '12-08-2025', Title: 'Notification of Acceptance' },
+    { Dates: '19-08-2025', Title: 'Camera-Ready Paper Submission' },
+    { Dates: '26-08-2025', Title: 'Last Date for Registration / Payment' },
+  ];
+
+  return (
+    <>
+      <section className="  relative overflow-hidden">
+        <section className="relative lg:h-[80vh] w-full "  >
+          <div className=" h-full w-full   bg-cover object-center z-0 flex items-center justify-center ">
+            <div className="  z-10 lg:pt-0 pt-20  lg:pb-0 pb-10 flex  justify-center items-center lg:gap-7 gap-5 styless   bg-gradient-to-r from-[#032530] to-[#0A3B47] " >
+              <div className=" grid grid-cols-1 lg:grid-cols-2 max-w-[90rem] mx-auto md:gap-10 gap-5 items-center lg:px-5">
+                <div className="lg:ml-auto mx-auto flex flex-col gap-6 lg:px-0 px-4">
+                  <h1 className="text-2xl 2xl:text-5xl lg:text-4xl font-bold leading-snug  md:text-left text-center text-white">
+                    International <span className=" text-[#4AF8BA]">Conference</span>  on Innovations in Advanced Computing and Software Engineering </h1>
+                  <div className=" md:mx-0 mx-auto">
+                    <Link to="/paper-submission">
+                      <button className=" bg-[#4AF8BA] duration-300 md:text-base text-sm rounded-full  font-semibold  text-[#032530] px-6 py-3   cursor-pointer   ">
+                        Register Now
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+                <div className="relative">
+                  <div className="flex  justify-center items-center lg:px-0 px-2 ">
+                    <img src="/assets/images/Hero-section.jpg" alt="Conference" rel="preload" className=" rounded-full z-30 lg:ml-auto mr-auto w-96 h-96  absolute p-5 lg:right-0 lg:left-auto    " />
+                    <img src="/assets/images/outline.png" alt="Conference" rel="preload" className=" lg:block hidden z-30 lg:ml-auto lg:mr-0   w-96 h-96  animate-oscillate" />
+                    <img src="/assets/images/second-one.png" alt="Conference" rel="preload" className=" lg:hidden block z-30 lg:ml-auto lg:mr-0   w-96 h-96  animate-oscillate" />
+                    <img src="/assets/images/side.jpg" alt="Conference" rel="preload" className=" rounded-full z-30 lg:block hidden  lg:ml-auto mr-auto w-24 h-24 absolute top-0  lg:right-auto     " />
+                    <img src="/assets/images/sidene.jpg" alt="Conference" rel="preload" className=" rounded-full lg:block hidden  z-30 lg:ml-auto mx-0 w-24 h-24 absolute bottom-24   -right-5  " />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* <div className=" px-10 pt-10 pb-3 bg-white w-fit rounded-full -bottom-10 left-40 z-50  absolute">
+          <i class="fi fi-ss-down text-black text-2xl"></i>
+        </div> */}
+      </section>
+      {/* <section className=" ">
+        <section className="max-w-[80rem] pt-7 md:pt-10 mx-auto md:px-5 px-3 flex flex-col md:gap-10 gap-5">
+          <section className="grid md:grid-cols-1 grid-cols-1 md:gap-5 gap-3 items-start">
+            <div className='flex flex-col md:gap-7 gap-3'>
+              <h1 className="text-center w-fit mx-auto rounded-full font-semibold lg:text-3xl text-xl bg-gradient-to-r p-1 from-[#2487DB] to-[#014BBD] bg-clip-text text-transparent">
+                About Us
+              </h1>
+              <div className="grid lg:grid-cols-2 md:gap-10 gap-5 items-center">
+                <img src="/assets/images/integration.jpg" className=" mx-auto" style={{ clipPath: 'polygon(0 0, calc(100% - 60px) 0, 100% 60px, 100% 100%, 60px 100%, 0 calc(100% - 60px))' }} alt="" />
+                <div>
+                  <div className=" flex flex-col gap-3 ">
+                    <p className="text-justify md:text-lg text-sm leading-7">
+                      Join us at the <strong>International Conference on Technology Integration for Global Development (ICTIGD)</strong> — a global forum committed to driving innovation through the integration of emerging technologies for sustainable and inclusive progress. This prestigious event brings together researchers, scholars, industry professionals, and students to share breakthroughs, discuss transformative trends, and present practical solutions that leverage technology for global development challenges.
+                      <br />
+                      ICTIGD provides a vibrant platform for knowledge exchange, cross-sector collaboration, and interdisciplinary exploration into how technologies like artificial intelligence, smart systems, automation, and data science can be harnessed to solve complex real-world issues and promote equitable growth.
+                      <br />
+                    </p>
+                    <div className="md:mx-0 mx-auto">
+                      <Link to="/about">
+                        <button className="font-semibold bg-white hover:bg-[#f0f4ff] duration-300 text-[#014BBD] md:text-base text-sm px-6 shadow-lg py-2 md:py-3  cursor-pointer">
+                          Read More
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="max-w-[80rem] mx-auto flex flex-col md:gap-7 gap-3">
+            <h1 className="text-center w-fit mx-auto rounded-full font-semibold lg:text-3xl text-xl p-1 bg-gradient-to-r from-[#2487DB] to-[#014BBD] bg-clip-text text-transparent">
+              Key Invitees
+            </h1>
+            <div className="grid md:grid-cols-4 grid-cols-1 md:gap-10 gap-5">
+              {invitees.map((member, index) => (
+                <Invitees key={index} Name={member.Name} Position={member.Position} location={member.location} dataAos="flip-left" />
+              ))}
+            </div>
+          </section>
+          <section className="  max-w-[80rem]  mx-auto  ">
+            <div className="  flex flex-col md:gap-7 gap-3">
+              <h1 className="text-center w-fit mx-auto  font-semibold lg:text-3xl text-xl p-1 bg-gradient-to-r from-[#2487DB] to-[#014BBD] bg-clip-text text-transparent">
+                Key Dates
+              </h1>
+              <div className="p-5 bg-gradient-to-tr shadow-lg   from-[#f0f4ff] to-[#e2ecfc]  " >
+                <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-10 gap-5  ">
+                  {date.map((item, index) => (
+                    <div key={index} style={{
+                      clipPath: 'polygon(0 0, calc(100% - 30px) 0, 100% 30px, 100% 100%, 30px 100%, 0 calc(100% - 30px))',
+                    }} className="  w-full h-full flex items-center     p-5 hover:shadow-xl transition duration-300 bg-white/80 backdrop-blur-sm"  >
+                      <KeyDates Dates={item.Dates} Title={item.Title} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="max-w-[100rem]  mx-auto   lg:w-1/2 w-full text-white">
+            <div className=" flex flex-col md:gap-7 gap-3">
+              <h1 className="text-center w-fit mx-auto font-semibold lg:text-3xl text-xl bg-gradient-to-r p-1 from-[#2487DB] to-[#014BBD] bg-clip-text text-transparent">
+                Reach Us
+              </h1>
+              <div className=" w-full">
+                <form onSubmit={handleSubmit} className="w-full grid lg:grid-cols-2 gap-5 bg-[#1B1F3B] p-5  " style={{ clipPath: 'polygon(0 0, calc(100% - 20px) 0, 100% 20px, 100% 100%, 20px 100%, 0 calc(100% - 20px))', }} ngNativeValidate>
+                  <div className="">
+                    <label for="email" className="block mb-2 text-sm mt-4 ">
+                      Name
+                    </label>
+                    <input type="text" name="name" id="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      ngModel className="shadow-sm   border text-sm   border-gray-300  bg-white text-black focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5  mt-4" placeholder="Enter Your Name" required />
+                  </div>
+                  <div>
+                    <label for="email" className="block mb-2 text-sm mt-4">
+                      Email
+                    </label>
+                    <input type="email" name="email" id="email" onChange={handleChange} value={formData.email} ngModel className="shadow-sm  border bg-white text-black   border-gray-300    text-sm   focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5   mt-4 "
+                      placeholder="name@gmail.com" required />
+                  </div>
+                  <div>
+                    <label for="email" className="block mb-2 text-sm      mt-4 "  >
+                      Contact Number
+                    </label>
+                    <input type="text" name="number" id="number" onChange={handleChange} value={formData.number} ngModel className="shadow-sm bg-white text-black  border   border-gray-300    text-sm   focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5   mt-4  "
+                      placeholder="Enter Your Mobile Number" required />
+                  </div>
+                  <div className="lg:col-span-2 mt-4">
+                    <label for="message" className="block mb-2 text-sm ">
+                      Your Message
+                    </label>
+                    <textarea id="message" name="message" value={formData.message} onChange={handleChange} ngModel
+                      rows="4"
+                      className="block p-2.5 w-full text-sm shadow-sm border bg-white text-black   border-gray-300   focus:ring-primary-500 focus:border-primary-500   mt-4 "
+                      placeholder="Leave a Message..." ></textarea>
+                  </div>
+                  <div className="text-center md:mt-0 mt-5 lg:col-span-2">
+                    <button type="submit" className="py-3 px-6    font-semibold   text-center  md:text-base text-sm shadow-xl   bg-white hover:bg-[#f0f4ff] duration-300  text-[#014BBD]  cursor-pointer     " >
+                      {status === 'Sending...' ? 'Submitting...' : 'Send message'}
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+          <LastSection />
+        </section>
+      </section > */}
+    </>
+  );
+};
+
+
+
